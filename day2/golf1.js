273bb3a82e5001c9796396379db56dd782c044f7
darksunlight <darksunlight210@gmail.com>
2022-12-01 22:02:12 -0800

    184

--- day2/golf1.js
+++ day2/golf1.js
@@ -1,2 +1,2 @@
-c=(x)=>x.charCodeAt(0);s=0;r=`${require('fs').readFileSync(0)}`.split`
-`.map(l=>{x=c('X');[o,p]=l.split(' ').map(x=>c(x));d=x-c('A');t=o-p+d;s+=p-x+1+(!t)*3+!(t+1&&t-2)*6;});console.log(s);
+c=x=>x[0].charCodeAt(0);s=0;r=`${require('fs').readFileSync(0)}`.split`
+`.map(l=>{x=c`X`;[o,p]=l.split` `.map(x=>c(x));d=x-c`A`;t=o-p+d;s+=p-x+1+(!t)*3+!(t+1&&t-2)*6;});console.log(s);
