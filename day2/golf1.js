26fcef68676b8cf529d59e9209ea6b4201fd4b3f
darksunlight <darksunlight210@gmail.com>
2022-12-01 22:08:53 -0800

    151

--- day2/golf1.js
+++ day2/golf1.js
@@ -1,2 +1,2 @@
-c=x=>x[0].charCodeAt(0);s=0;r=`${require('fs').readFileSync(0)}`.split`
-`.map(l=>{x=c`X`;[o,p]=l.split` `.map(x=>c(x));d=x-c`A`;t=o-p+d;s+=p-x+1+(!t)*3+!(t+1&&t-2)*6});console.log(s)
+s=0;r=`${require('fs').readFileSync(0)}`.split`
+`.map(l=>{[o,p]=l.split` `.map(n=>n.charCodeAt(0));t=o-p+23;s+=p-87+!t*3+!(t+1&&t-2)*6});console.log(s)
