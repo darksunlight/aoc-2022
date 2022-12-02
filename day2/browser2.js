ab72f89ac20229245d26403a0a0d28d08958faa3
darksunlight <darksunlight210@gmail.com>
2022-12-02 00:28:23 -0800

    85!!!

---
+++ day2/browser2.js
@@ -0,0 +1,2 @@
+s=0;prompt().split`
+`.map(w=>s+='__BXCXAXAYBYCYCZAZBZ'.indexOf(w[0]+w[2]));alert(s/2)
--- day2/golf2.js
+++ day2/golf2.js
@@ -1,2 +1,2 @@
-console.log(`${require('fs').readFileSync(0)}`.split`
-`.reduce((s,w)=>s+'__BXCXAXAYBYCYCZAZBZ'.indexOf(w[0]+w[2]),0)/2)
+s=0;`${require('fs').readFileSync(0)}`.split`
+`.map(w=>s+='__BXCXAXAYBYCYCZAZBZ'.indexOf(w[0]+w[2]));console.log(s/2)
