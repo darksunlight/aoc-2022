63d466186f634817239a89ce9ef63a6c13e526bf
darksunlight <darksunlight210@gmail.com>
2022-12-01 22:15:57 -0800

    149 btw I tried .reduce and it's 10 chars longer

--- day2/golf1.js
+++ day2/golf1.js
@@ -1,2 +1,2 @@
-s=0;r=`${require('fs').readFileSync(0)}`.split`
+s=0;`${require('fs').readFileSync(0)}`.split`
 `.map(l=>{[o,p]=l.split` `.map(n=>n.charCodeAt(0));t=o-p+23;s+=p-87+!t*3+!(t+1&&t-2)*6});console.log(s)
