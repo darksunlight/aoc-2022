bc9e9bea6eeae6ae7457b2b990126ec7d50b4ec1
darksunlight <darksunlight210@gmail.com>
2022-12-04 23:47:02 -0800

    1 char saved

--- day5/golf1.js
+++ day5/golf1.js
@@ -3,5 +3,5 @@
 `.map(x=>x.split`
 `)
 s=[...Array(h=d[l=d.length-1].split`   `.length)].map(_=>[]);[...Array(h).keys()].map(i=>{for(j=l-1;j+1;j--)(e=d[j][4*i+1])!=' '&&s[i].push(e)})
-p.map(r=>{[,q,,o,,t]=r.split` `.map(x=>+x);[...Array(q)].map(_=>s[t-1].push(s[o-1].pop()))})
+p.map(r=>{[,q,,o,,t]=r.split` `.map(x=>x-1);[...Array(q+1)].map(_=>s[t].push(s[o].pop()))})
 console.log(s.map(x=>x[x.length-1]).join(``))
