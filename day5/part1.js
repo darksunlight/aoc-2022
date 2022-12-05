ceb296255b44ed3f2d75be9073373c7c138f69a3
darksunlight <darksunlight210@gmail.com>
2022-12-04 23:12:57 -0800

    day 5 proper parsing

--- day5/part1.js
+++ day5/part1.js
@@ -1,16 +1,13 @@
 require("../utils");
-const [, procedures] = readInput().split('\n\n').map(x => x.split('\n'));
-const stacks = [
-    'FDBZTJRN',
-    'RSNJH',
-    'CRNJGZFQ',
-    'FVNGRTQ',
-    'LTQF',
-    'QCWZBRGN',
-    'FCLSNHM',
-    'DNQMTJ',
-    'PGS',
-].map(x => [...x]);
+const [drawing, procedures] = readInput().split('\n\n').map(x => x.split('\n'));
+const stacks = Array(drawing[drawing.length-1].trim().split('   ').length).fill(0).map(_ => []);
+for (let i = 0; i < stacks.length; i++) {
+    for (let j = drawing.length - 2; j >= 0; j--) {
+        const el = drawing[j][4*i+1];
+        if (el === ' ') break;
+        stacks[i].push(el);
+    }
+}
 procedures.map(p => {
     const [, quantity, , orig, , dest] = p.split(' ').map(x => +x);
     for (let i = 0; i < quantity; i++) {
