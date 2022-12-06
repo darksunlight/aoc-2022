89e1da549222e0beefa739c4ffd005474b6570ac
darksunlight <darksunlight210@gmail.com>
2022-12-06 01:03:47 -0800

    remove useless brackets

--- day6/part1.js
+++ day6/part1.js
@@ -1,3 +1,3 @@
 require("../utils");
 const input = readInput();
-print(input.indexOf(input.split('').windowed(4).filter(x => (new Set(x).size === 4))[0].join(''))+4)
+print(input.indexOf(input.split('').windowed(4).filter(x => new Set(x).size === 4)[0].join(''))+4)
--- day6/part2.js
+++ day6/part2.js
@@ -1,3 +1,3 @@
 require("../utils");
 const input = readInput();
-print(input.indexOf(input.split('').windowed(14).filter(x => (new Set(x).size === 14))[0].join(''))+14)
+print(input.indexOf(input.split('').windowed(14).filter(x => new Set(x).size === 14)[0].join(''))+14)
