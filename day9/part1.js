620fa954f4464f99625c717e5d3643e5e94540bf
darksunlight <darksunlight210@gmail.com>
2022-12-08 22:14:36 -0800

    more day 9 cleanup

--- day9/part1.js
+++ day9/part1.js
@@ -5,20 +5,16 @@ function tooFar(h, t) {
 }
 let head = { x: 0, y: 0 };
 let tail = { x: 0, y: 0 };
-const beenTo = new Set();
-beenTo.add(`${tail.x},${tail.y}`);
+const visited = new Set();
+visited.add(`${tail.x},${tail.y}`);
 input.map(line => {
     for (let i = 0; i < line[1]; i++) {
         head['LR'.indexOf(line[0]) > -1 ? 'x' : 'y'] += 'LD'.indexOf(line[0]) > -1 ? -1 : 1;
         if (tooFar(head, tail)) {
-            if (head.y == tail.y) tail.x += head.x > tail.x ? 1 : -1;
-            else if (head.x == tail.x) tail.y += head.y > tail.y ? 1 : -1;
-            else {
-                tail.x += head.x > tail.x ? 1 : -1;
-                tail.y += head.y > tail.y ? 1 : -1;
-            }
-            beenTo.add(`${tail.x},${tail.y}`);
+            if (head.y !== tail.y) tail.y += head.y > tail.y ? 1 : -1;
+            if (head.x !== tail.x) tail.x += head.x > tail.x ? 1 : -1;
+            visited.add(`${tail.x},${tail.y}`);
         }
     }
 });
-console.log(beenTo.size)
+console.log(visited.size)
--- day9/part2.js
+++ day9/part2.js
@@ -16,8 +16,8 @@ const knots = [
     { x: 0, y: 0 },
     { x: 0, y: 0 },
 ];
-const beenTo = new Set();
-beenTo.add('0,0');
+const visited = new Set();
+visited.add('0,0');
 input.map(line => {
     for (let i = 0; i < line[1]; i++) {
         [0,1,2,3,4,5,6,7,8,9].windowed(2).forEach(pair => {
@@ -25,15 +25,11 @@ input.map(line => {
             const tail = knots[pair[1]];
             if (pair[0] === 0) head['LR'.indexOf(line[0]) > -1 ? 'x' : 'y'] += 'LD'.indexOf(line[0]) > -1 ? -1 : 1;
             if (tooFar(head, tail)) {
-                if (head.y == tail.y) tail.x += head.x > tail.x ? 1 : -1;
-                else if (head.x == tail.x) tail.y += head.y > tail.y ? 1 : -1;
-                else {
-                    tail.x += head.x > tail.x ? 1 : -1;
-                    tail.y += head.y > tail.y ? 1 : -1;
-                }
-                if (pair[1] === 9) beenTo.add(`${tail.x},${tail.y}`);
+                if (head.y !== tail.y) tail.y += head.y > tail.y ? 1 : -1;
+                if (head.x !== tail.x) tail.x += head.x > tail.x ? 1 : -1;
+                if (pair[1] === 9) visited.add(`${tail.x},${tail.y}`);
             }
         })
     }
 });
-console.log(beenTo.size)
+console.log(visited.size)
