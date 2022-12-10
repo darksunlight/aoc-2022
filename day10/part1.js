e3599b5ee69766ac60a2dcf611e322115face874
darksunlight <darksunlight210@gmail.com>
2022-12-09 23:08:04 -0800

    day 10 simplify

--- day10/part1.js
+++ day10/part1.js
@@ -7,15 +7,13 @@ input.forEach(line => {
     if ((cycle - 20) % 40 === 0) {
         sum += x * cycle;
     }
-    if (line === "noop") {
-        return cycle++;
+    if (line !== "noop") {
+        cycle++;
+        if ((cycle - 20) % 40 === 0) {
+            sum += x * cycle;
+        }
+        x += +line.split(' ')[1];
     }
-    const inc = +line.split(' ')[1];
     cycle++;
-    if ((cycle - 20) % 40 === 0) {
-        sum += x * cycle;
-    }
-    cycle++;
-    x += inc;
 });
 console.log(sum);
